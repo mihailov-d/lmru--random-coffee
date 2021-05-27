e0eedb82fd15dfc0fa0cf7622e2333edf330ac15
Dmitry Korotkin <dmitry.korotkin@leroymerlin.ru>
2021-05-28 01:00:28 +0300

    Add Schemas

---
+++ src/main/kotlin/ru/leroymerlin/random/coffee/core/model/Schema.kt
@@ -0,0 +1,35 @@
+package ru.leroymerlin.random.coffee.core.model
+
+import nonapi.io.github.classgraph.json.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+
+@Document data class User(
+    @Id val id: String,
+    val info:   Info,
+    val company:    Company,
+    val contact:    Contact,
+    val meetInfo:   meetInfo
+    )
+@Document data class Company(
+    @Id val id: String,
+    val name:   String,
+    val position:   String
+)
+@Document data class Contact(
+    @Id val id: String,
+    val email:  String,
+    val phone: String
+)
+
+@Document data class Info(
+    @Id val id: String,
+    val firstName:  String,
+    val surName: String
+)
+@Document data class meetInfo(
+    @Id val id: String,
+    val date:   LocalDate,
+    val name:   String
+)
+
