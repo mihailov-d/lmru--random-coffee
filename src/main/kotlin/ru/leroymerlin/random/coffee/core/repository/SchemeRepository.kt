3b848db0cef8758de6cefa837b332a5dc862d9a2
Dmitry Korotkin <dmitry.korotkin@leroymerlin.ru>
2021-05-28 16:08:25 +0300

    find by UUID

--- src/main/kotlin/ru/leroymerlin/random/coffee/core/repository/SchemeRepository.kt
+++ src/main/kotlin/ru/leroymerlin/random/coffee/core/repository/SchemeRepository.kt
@@ -5,20 +5,21 @@ import org.springframework.stereotype.Repository
 import ru.leroymerlin.random.coffee.core.model.Meeting
 import ru.leroymerlin.random.coffee.core.model.Session
 import ru.leroymerlin.random.coffee.core.model.User
+import java.util.UUID
 
 @Repository
 interface UserRepository : MongoRepository<User, String> {
-    fun findOneById(id: String): User
+    fun findOneById(id: UUID): User
 }
 
 @Repository
 interface MeetingRepository : MongoRepository<Meeting, String> {
-    fun findOneById(id: String): Meeting
+    fun findOneById(id: UUID): Meeting
 }
 
 @Repository
 interface SessionRepository : MongoRepository<Session, String> {
-    fun findOneById(id: String): Session
+    fun findOneById(id: UUID): Session
 }
 
 
