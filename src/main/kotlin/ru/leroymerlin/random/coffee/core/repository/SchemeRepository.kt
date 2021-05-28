f0f8e888ea59468580b75ee1c9a54b23a378d262
Dmitrii Mihailov <dmitrii.mihailov@leroymerlin.ru>
2021-05-28 16:16:18 +0300

    fix: Change codec for UUID

--- src/main/kotlin/ru/leroymerlin/random/coffee/core/repository/SchemeRepository.kt
+++ src/main/kotlin/ru/leroymerlin/random/coffee/core/repository/SchemeRepository.kt
@@ -2,9 +2,7 @@ package ru.leroymerlin.random.coffee.core.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import ru.leroymerlin.random.coffee.core.model.Job
 import ru.leroymerlin.random.coffee.core.model.User
-import java.util.UUID
 
 @Repository
 interface UserRepository : MongoRepository<User, String> {
