015def9e34a9f7ba86c9ac46503750564805b265
sergey.samohvalov <sergey.samohvalov@leroymerlin.ru>
2021-05-28 21:10:55 +0300

    hide keyboard

--- src/main/kotlin/ru/leroymerlin/random/coffee/core/tg/ability/MeetingMatchAbility.kt
+++ src/main/kotlin/ru/leroymerlin/random/coffee/core/tg/ability/MeetingMatchAbility.kt
@@ -3,13 +3,19 @@ package ru.leroymerlin.random.coffee.core.tg.ability
 import org.springframework.stereotype.Component
 import org.telegram.abilitybots.api.objects.Reply
 import org.telegram.abilitybots.api.util.AbilityExtension
-import ru.leroymerlin.random.coffee.core.util.chatId
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove
+import ru.leroymerlin.random.coffee.core.util.stringChatId
 import ru.leroymerlin.random.coffee.core.util.textEquals
 
 @Component
 class MeetingMatchAbility : AbilityExtension {
     fun publishMeetingReply(): Reply = Reply.of({ b, update ->
-        b.silent().sendMd("Встреча опубликована", update.chatId())
+        val message = SendMessage()
+        message.replyMarkup = ReplyKeyboardRemove(true)
+        message.chatId = update.stringChatId()
+        message.text = "Встреча опубликована"
+        b.execute(message)
         // TODO meeting publicate logic
     }, textEquals(CommandList.MATCH_MEETING_START.command))
 }
